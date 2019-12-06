f57fe329964c9a9377e4d5438ab34b43eab6d76c
Ryaner1994 <xuewyan@foxmail.com>
2019-12-06 21:04:44 +0800

    i2c debug success and Mxl241 Successful

--- NUC980-linux-4.4.y/arch/arm/boot/.uImage.cmd
+++ NUC980-linux-4.4.y/arch/arm/boot/.uImage.cmd
@@ -1,1 +1,1 @@
-cmd_arch/arm/boot/uImage := /bin/sh ./scripts/mkuboot.sh -A arm -O linux -C none  -T kernel -a 0x00008000 -e 0x00008000 -n 'Linux-4.4.179+' -d arch/arm/boot/zImage arch/arm/boot/uImage
+cmd_arch/arm/boot/uImage := /bin/sh ./scripts/mkuboot.sh -A arm -O linux -C none  -T kernel -a 0x00008000 -e 0x00008000 -n 'Linux-4.4.179' -d arch/arm/boot/zImage arch/arm/boot/uImage
